20a08f18c38c5fef2c1691be838a1c0566a2601c
Khrystyna Oliinyk <khrystynaa.ol@gmail.com>
2025-09-17 12:35:27 +0300

    Виправлення: В 2 завданні додано пробіли до паттерну В 4 завданні додана обробка помилки, якщо користувач нічого не ввів

--- task_4.py
+++ task_4.py
@@ -87,24 +87,26 @@ def main():
     contacts = {}
     print("Welcome to the assistant bot!", 'Commands: ', commands, sep='\n')
     while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
-
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == 'change':
-            print(change_username_phone(args, contacts))
-        elif command == 'phone':
-            print(show_phone(args, contacts))
-        elif command == 'all':
-            print(show_all(contacts))
-        else:
-            print("Invalid command.")
+        try:
+            user_input = input("Enter a command: ")
+            command, *args = parse_input(user_input)
+            if command in ["close", "exit"]:
+                print("Good bye!")
+                break
+            elif command == "hello":
+                print("How can I help you?")
+            elif command == "add":
+                print(add_contact(args, contacts))
+            elif command == 'change':
+                print(change_username_phone(args, contacts))
+            elif command == 'phone':
+                print(show_phone(args, contacts))
+            elif command == 'all':
+                print(show_all(contacts))
+            else:
+                print("Invalid command.")
+        except ValueError as e:
+            print('Please choose a command')
 
 if __name__ == "__main__":
     main()
